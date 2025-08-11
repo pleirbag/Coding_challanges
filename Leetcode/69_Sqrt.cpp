b3e8974504fcf42c44a9827c3621eda13ff8b610
pleirs pc <gab.piccini@gmail.com>
2025-08-11 01:20:16 +0100

    added leetcode number 69

---
+++ Leetcode/69_Sqrt.cpp
@@ -0,0 +1,19 @@
+/*Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+
+You must not use any built-in exponent function or operator.
+
+    For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+*/
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        double result  {0};
+
+        if (x == 1)
+            return (1);
+        while (((result + 1) * (result + 1)) <= x)
+            result++;
+        return ((int)result);        
+    }
+};
